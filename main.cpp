e33b16aab2a62a0bc9531a8e58ae2e1eea6e910c
sild <failsild@gmail.com>
2014-05-10 02:21:19 +0400

    copy file added

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include <iostream>
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    std::cout << "lol" << std::endl;
-    exit(1);
-    return a.exec();
-}
