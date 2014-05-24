78a3046ab060b22dc2a23dc81b29f74305ab426b
sild <failsild@gmail.com>
2014-05-24 21:44:08 +0400

    fix use md5

--- source/filemanager.cpp
+++ source/filemanager.cpp
@@ -67,14 +67,15 @@ bool Filemanager::segmentate(std::string target_file_name) {
         if(ifile.gcount()) {
 
             const char* enc_buffer = this->scrambler->encode(buffer, ifile.gcount());
-
-            metarow = md5(buffer) + " " + md5(enc_buffer) + " " + std::to_string(read_start) + " " + std::to_string(read_start+ifile.gcount());   
+            std::string str_buffer(buffer, ifile.gcount());
+            std::string enc_str_buffer(enc_buffer, ifile.gcount());
+            metarow = md5(str_buffer) + " " + md5(enc_str_buffer) + " " + std::to_string(read_start) + " " + std::to_string(read_start+ifile.gcount());   
             chang_path = this->output_dir + "/" + md5(enc_buffer);
             std::ofstream ofile( chang_path.c_str(), std::ios::binary);
             read_start += ifile.gcount();  
 
             if(!ofile){ //ошибку открытия файлов стоит отслеживать, всякое бывает
-                std::cout << "cannot open output files named \"" << md5(enc_buffer) << "\" \n";
+                std::cout << "cannot open output files named \"" << md5(enc_str_buffer) << "\" \n";
                 return false;
             }
 
